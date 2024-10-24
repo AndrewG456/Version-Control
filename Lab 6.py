3dca1476762084837087f86864a4f730da162b67
kanjula Amarasekera <minaya.na196@gmail.com>
2024-10-23 22:12:27 -0400

    decoded function + changes to main responses - Minaya

--- Lab 6.py
+++ Lab 6.py
@@ -7,8 +7,12 @@ def encode():
         new_password  += str((int(password[i]) + 3) % 10)
     return new_password
 
-def decode():
-    pass
+def decode(digits):
+    old_password = ''
+    for i in range(len(digits)):
+        a = (int(digits[i])-3 + 10)%10
+        old_password += str(a)
+    return old_password
 
 if __name__ == '__main__':
 
@@ -21,8 +25,10 @@ if __name__ == '__main__':
         print('3.\tQuit\n')
         choice = input("Please enter an option: ")
         if choice == '1':
-            encode()
+            encoded_password = encode()
         elif choice == '2':
-            decode()
+            decoded_password = decode(encoded_password)
+            print("The encoded password is", encoded_password, ",", end="")
+            print("and the original password is", decoded_password)
         elif choice == '3':
             break
